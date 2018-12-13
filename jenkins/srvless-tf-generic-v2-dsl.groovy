f4ec19e0caa26a70c13550415df21c33b31135ad
Sebastian Mincewicz <s.mincewicz@kainos.com>
2018-12-13 16:43:13 +0000

    dsl

---
+++ jenkins/srvless-tf-generic-v2-dsl.groovy
@@ -0,0 +1,29 @@
+pipelineJob('GenericDeclarativePipeline') {
+  displayName('Generic Declarative Pipeline')
+  scm {
+    git{
+      remote {
+        url('https://github.com/sebolabs/pipelines.git')
+        branch('jenkins-pipeline-v2')
+      }
+    }
+  }
+  steps {
+    dsl {
+      external('srvless-tf-generic-v2-dsl.groovy')
+      removeAction('DELETE')
+      removeViewAction('DELETE')
+    }
+  }
+  parameters {
+    activeChoiceParam('ENVIRONMENT') {
+      choiceType('SINGLE_SELECT')
+      groovyScript {
+          script("return ['INT', 'PREP', 'UAT', 'LIVE']")
+      }
+    }
+    stringParam('TRANSCODER_BRANCH', 'master', '')
+    stringParam('IMAGES_BRANCH', 'master', '')
+    stringParam('TERRAFORM_BEANCH', 'terraform-mock', '')
+  }
+}
