e3dedca4c23dce5d79a60c67aed4b4990e9305ef
Sebastian Mincewicz <s.mincewicz@kainos.com>
2018-12-14 12:20:11 +0100

    WIP

--- jenkins/srvless_tf_generic_v2_pipeline.groovy
+++ jenkins/srvless_tf_generic_v2_pipeline.groovy
@@ -2,10 +2,12 @@ pipelineJob('generic_declarative_pipeline') {
   displayName('Generic Declarative Pipeline')
   description('A generic declarative pipeline for Lambda powered APIs')
   definition {
-    scm {
-      git {
-        remote {
-          url('https://github.com/sebolabs/pipelines.git')
+    cpsScm {
+      scm {
+        git {
+          remote {
+            url('https://github.com/sebolabs/pipelines.git')
+          }
           branch('${PIPELINE_BRANCH}') // TODO: change to variable?
         }
       }
