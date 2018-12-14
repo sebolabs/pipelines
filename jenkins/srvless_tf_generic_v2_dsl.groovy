56f569249977a677de804f4c982aa2d7351e15d9
Sebastian Mincewicz <s.mincewicz@kainos.com>
2018-12-14 11:32:34 +0100

    WIP

--- jenkins/srvless_tf_generic_v2_dsl.groovy
+++ jenkins/srvless_tf_generic_v2_dsl.groovy
@@ -16,12 +16,7 @@ pipelineJob('GenericDeclarativePipeline') {
     }
   }
   parameters {
-    activeChoiceParam('ENVIRONMENT') {
-      choiceType('SINGLE_SELECT')
-      groovyScript {
-          script("return ['INT', 'PREP', 'UAT', 'LIVE']")
-      }
-    }
+    choiceParam('ENVIRONMENT', ['INT', 'PREP', 'UAT', 'LIVE'])
     stringParam('TRANSCODER_BRANCH', 'master', '')
     stringParam('IMAGES_BRANCH', 'master', '')
     stringParam('TERRAFORM_BEANCH', 'terraform-mock', '')
